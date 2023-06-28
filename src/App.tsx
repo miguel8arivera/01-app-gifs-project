b6094f47937479bc99dfe4a76be818eafe6ff579
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-06-27 22:26:55 -0500

    app.tsx modificated

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
     <>
       <div>
         <h3>Hello world</h3>
+        <h2>
+          Solucionar la parte de consumir lo de giphy y terminar el projecto el
+          28 de junio y hacer el test
+        </h2>
       </div>
     </>
   );
